b664921744c3f4e42fc0b195ee3c6d153044ea89
Xiaochen Xu <xiaochex@gmail.com>
2021-02-25 09:11:05 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
